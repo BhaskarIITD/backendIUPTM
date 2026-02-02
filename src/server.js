2bf436d3bece8af278f8d73f57c462155a2082de
Shridhar Ashutosh Roy <ashu@MacBook-Air-6.local>
2026-02-03 02:56:01 +0530

    fixed

--- src/server.js
+++ src/server.js
@@ -10,7 +10,22 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
-app.use(cors());
+
+const allowedOrigins = [
+  "https://frontend-iutm.vercel.app",
+  "http://localhost:5173"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 
 app.use(express.json());
