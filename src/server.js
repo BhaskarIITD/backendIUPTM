e32b8884e8007572eb8512c0863f641cd419036b
Shridhar Ashutosh Roy <ashu@MacBook-Air-6.local>
2026-02-03 02:34:08 +0530

    Fix Bug

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
